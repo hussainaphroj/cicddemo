c17a2a676068a73202d42df3f5c8847c4854c747
Afroz Hussain <hussainaphroj@gmail.com>
2023-06-06 22:18:54 +0400

    Added Ansible playbook for k8 deployment

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -27,5 +27,13 @@ pipeline{
         }
       }
     }
+    stage ('Deploy') {
+          steps {
+              script{
+                  def image_id = registry + ":$BUILD_NUMBER"
+                  sh "ansible-playbook playbook.yml --extra-vars \"image_id=${image_id}\""
+              }
+          }
+      }
 }
 }
